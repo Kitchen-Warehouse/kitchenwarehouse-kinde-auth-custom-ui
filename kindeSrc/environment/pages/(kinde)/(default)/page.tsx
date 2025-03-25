a7d66fe47207b3466a641e6ce16b60c55ac31891
ankush <ankush@compose.co.in>
2025-03-25 12:10:59 +0530

    initialize: Next.js project with Kinde authentication and Tailwind CSS setup

---
+++ kindeSrc/environment/pages/(kinde)/(default)/page.tsx
@@ -0,0 +1,58 @@
+'use server';
+
+import {
+  getDarkModeLogoUrl,
+  getKindeWidget,
+  getLogoUrl,
+  type KindePageEvent,
+} from '@kinde/infrastructure';
+import React from 'react';
+// @ts-expect-error: renderToString is not available in the server environment
+import { renderToString } from 'react-dom/server.browser';
+import Layout from '../../layout';
+
+const DefaultPage: React.FC<KindePageEvent> = ({ context, request }) => {
+  return (
+    <Layout context={context} request={request}>
+      <div className='container'>
+        <main className='login-form-wrapper'>
+          <div className='login-form'>
+            <div className='logo-wrapper'>
+              <picture>
+                <source
+                  media='(prefers-color-scheme: dark)'
+                  srcSet={getDarkModeLogoUrl()}
+                />
+                <img
+                  className='logo'
+                  src={getLogoUrl()}
+                  alt={context.widget.content.logoAlt}
+                  width={152}
+                  height={32}
+                />
+              </picture>
+            </div>
+            {context.widget.content.heading && (
+              <h2 className='heading'>{context.widget.content.heading}</h2>
+            )}
+            <p className='description'>{context.widget.content.description}</p>
+            {getKindeWidget()}
+          </div>
+        </main>
+        <div className='side-panel'>
+          <img
+            className='side-panel-image'
+            src='https://media.kitchenwarehouse.com.au/image/upload/WebsiteContent/2025-01-17-d4793eb573e1ad22c3f46ea871e6471f.png'
+            alt='image'
+          />
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+// Page Component
+export default async function Page(event: KindePageEvent): Promise<string> {
+  const page = await DefaultPage(event);
+  return renderToString(page);
+}
---
+++ kindeSrc/environment/pages/layout.tsx
@@ -0,0 +1,139 @@
+'use server';
+
+import {
+  getKindeCSRF,
+  getKindeNonce,
+  getKindeRequiredCSS,
+  getKindeRequiredJS,
+  type KindePageEvent,
+} from '@kinde/infrastructure';
+import React from 'react';
+
+interface LayoutProps extends KindePageEvent {
+  children: React.ReactNode;
+}
+
+export const Layout = ({
+  request,
+  context,
+  children,
+}: LayoutProps): React.JSX.Element => {
+  return (
+    <html lang={request.locale.lang}>
+      <head>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta name='robots' content='noindex' />
+        <meta name='csrf-token' content={getKindeCSRF()} />
+        <title>{context.widget.content.pageTitle}</title>
+        {getKindeRequiredCSS()}
+        {getKindeRequiredJS()}
+        <link rel='stylesheet' href='/style.css'></link>
+        <style nonce={getKindeNonce()}>
+          {`
+           .container {
+              display: flex;
+              width: 100%;
+              max-width: 1920px;
+              justify-content: center;
+              margin: 0 auto;
+              @media screen and (max-width: 1023px) {
+                flex-direction: column;
+                padding: 20px;
+              }
+            }
+
+            .login-form-wrapper {
+              display: flex;
+              width: 100%;
+              gap: 120px;
+              justify-content: center;
+              flex: 1 0 0;
+              padding: 176px 0px;
+              @media screen and (max-width: 1023px) {
+                padding: 48px 0;
+              }
+            }
+
+            .login-form {
+              display: flex;
+              flex-direction: column;
+              gap: 24px;
+              width: 100%;
+              max-width: 480px;
+              font-family: Inter, sans-serif;
+            }
+
+            .logo-wrapper {
+              display: flex;
+              justify-content: center;
+            }
+
+            .description {
+              color: #1A1A1A;
+              line-height: 150%;
+              letter-spacing: -0.16px;
+            }
+
+            .kinde-control-label {
+              color: #1A1A1A;
+              font-size: 14px;
+              line-height: 20px;
+              letter-spacing: -0.14px;
+              padding-block-end: 4px;
+            }
+
+            .kinde-button-text {
+              font-size: 18px;
+              line-height: 28px;
+              letter-spacing: -0.36px;
+              font-weight: 600;
+            }
+
+            .kinde-choice-separator {
+              color: #1A1A1A;
+              line-height: 150%;
+              letter-spacing: -0.16px;
+            }
+
+            .kinde-fallback-action {
+              color: #1A1A1A;
+              line-height: 150%;
+              letter-spacing: -0.16px;
+            }
+
+            kinde-text-link kinde-text-link-is-inline {
+              color: #1A1A1A;
+              line-height: 150%;
+              letter-spacing: -0.16px;
+            }
+
+            .kinde-text-link {
+              font-weight: 600;
+            }
+
+            .side-panel {
+              flex: 1 0 0;
+                @media screen and (max-width: 1023px) {
+                    display: none;
+                }
+              }
+              .side-panel-image {
+                width: 100%;
+                height: 100vh;
+                object-fit: cover;
+              }
+            }
+          `}
+        </style>
+      </head>
+      <body>
+        <div data-roast-root='true' data-kinde-root='true'>
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+};
+
+export default Layout;
