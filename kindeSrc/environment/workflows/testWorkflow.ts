098d17b221ab34d79089d9f3526455c2f94f5f01
ankush <ankush@compose.co.in>
2025-03-27 13:47:36 +0530

    feat: update test workflow to use accessTokenCustomClaims and set customerId

--- kindeSrc/environment/workflows/testWorkflow.ts
+++ kindeSrc/environment/workflows/testWorkflow.ts
@@ -1,19 +1,25 @@
-import { WorkflowTrigger, idTokenCustomClaims } from "@kinde/infrastructure";
+import {
+  WorkflowTrigger,
+  accessTokenCustomClaims,
+} from '@kinde/infrastructure';
 
 export const workflowSettings = {
-    id: "addAccessTokenClaim",
-    trigger: WorkflowTrigger.UserTokenGeneration,
-    bindings: {
-      "kinde.accessToken": {},
-      "kinde.localization": {},
-      "kinde.fetch": {},
-      "kinde.env": {},
-      "kinde.mfa": {},
-      url: {},
-    },
-  };
+  id: 'addAccessTokenClaim',
+  trigger: WorkflowTrigger.UserTokenGeneration,
+  bindings: {
+    'kinde.accessToken': {},
+    'kinde.localization': {},
+    'kinde.fetch': {},
+    'kinde.env': {},
+    'kinde.mfa': {},
+    url: {},
+  },
+};
 
-  export default async function TestWorkflow() {
-    const idToken = idTokenCustomClaims()
-    console.log({idToken})
-  }
+export default async function TestWorkflow() {
+  const accessToken = accessTokenCustomClaims<{
+    customerId: string;
+  }>();
+
+  accessToken.customerId = '03dead9c-42fe-4f54-9638-d34ecfce694a';
+}
