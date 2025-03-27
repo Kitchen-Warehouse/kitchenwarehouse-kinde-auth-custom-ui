9f0c9cb367f9267bacb5506d3f87eb1bebf27636
ankush <ankush@compose.co.in>
2025-03-27 12:31:25 +0530

    refactor: standardize quotes and improve logging in test workflow

--- kindeSrc/environment/workflows/testWorkflow.ts
+++ kindeSrc/environment/workflows/testWorkflow.ts
@@ -1,22 +1,22 @@
-import { WorkflowTrigger, idTokenCustomClaims } from '@kinde/infrastructure';
+import { WorkflowTrigger, idTokenCustomClaims } from "@kinde/infrastructure";
 
 export const workflowSettings = {
-  id: 'addAccessTokenClaim',
-  trigger: WorkflowTrigger.UserTokenGeneration,
-  bindings: {
-    'kinde.accessToken': {},
-    'kinde.localization': {},
-    'kinde.fetch': {},
-    'kinde.env': {},
-    'kinde.mfa': {},
-    url: {},
-  },
-};
+    id: "addAccessTokenClaim",
+    trigger: WorkflowTrigger.UserTokenGeneration,
+    bindings: {
+      "kinde.accessToken": {},
+      "kinde.localization": {},
+      "kinde.fetch": {},
+      "kinde.env": {},
+      "kinde.mfa": {},
+      url: {},
+    },
+  };
 
-export default async function TestWorkflow() {
-  const idToken = idTokenCustomClaims<{
-    customerId: string;
-  }>();
-  console.log(idToken);
-  idToken.customerId = '03dead9c-42fe-4f54-9638-d34ecfce694a';
-}
+  export default async function TestWorkflow() {
+    const idToken = idTokenCustomClaims<{
+        customerId: string
+    }>()
+    console.log({idToken})
+    // idToken.customerId = "03dead9c-42fe-4f54-9638-d34ecfce694a"
+  }
