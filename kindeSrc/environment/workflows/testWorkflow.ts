e7b46f4b8980547c2f56646efa8f51d0d35d87e7
ankush <ankush@compose.co.in>
2025-04-01 21:47:22 +0530

    refactor: update TestWorkflow to change workflow ID and trigger for M2M token generation

--- kindeSrc/environment/workflows/testWorkflow.ts
+++ kindeSrc/environment/workflows/testWorkflow.ts
@@ -1,32 +1,47 @@
 import {
-  WorkflowTrigger,
-  accessTokenCustomClaims,
+  // WorkflowTrigger,
+  // accessTokenCustomClaims,
   createKindeAPI,
 } from '@kinde/infrastructure';
 
+// export const workflowSettings = {
+//   id: 'addAccessTokenClaim',
+//   trigger: WorkflowTrigger.UserTokenGeneration,
+//   bindings: {
+//     'kinde.accessToken': {},
+//     'kinde.localization': {},
+//     'kinde.fetch': {},
+//     'kinde.env': {},
+//     'kinde.mfa': {},
+//     url: {},
+//   },
+// };
+
 export const workflowSettings = {
-  id: 'addAccessTokenClaim',
-  trigger: WorkflowTrigger.UserTokenGeneration,
+  id: "m2mTokenGeneration",
+  name: "M2M custom claims",
+  failurePolicy: {
+    action: "stop",
+  },
+  trigger: "m2m:token_generation",
   bindings: {
-    'kinde.accessToken': {},
-    'kinde.localization': {},
-    'kinde.fetch': {},
-    'kinde.env': {},
-    'kinde.mfa': {},
-    url: {},
+    "kinde.m2mToken": {}, // required to modify M2M access token
+    "kinde.fetch": {}, // Required for external API calls
+    "kinde.env": {}, // required to access your environment variables
+    url: {}, // required for url params
   },
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default async function TestWorkflow(event:any) {
-  const accessToken = accessTokenCustomClaims<{
-    email: string;
-    user_properties: {
-      customer_id: {
-        v: string;
-      };
-    };
-  }>();
+  // const accessToken = accessTokenCustomClaims<{
+  //   email: string;
+  //   user_properties: {
+  //     customer_id: {
+  //       v: string;
+  //     };
+  //   };
+  // }>();
 
   const kindeAPI = await createKindeAPI(event);
 
