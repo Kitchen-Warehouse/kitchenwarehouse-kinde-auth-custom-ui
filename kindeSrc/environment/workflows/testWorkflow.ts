cd04f8d47218aa9117785417cb978085190271b2
ankush <ankush@compose.co.in>
2025-04-01 22:01:14 +0530

    refactor: update TestWorkflow to use new workflow ID and trigger for user token generation

--- kindeSrc/environment/workflows/testWorkflow.ts
+++ kindeSrc/environment/workflows/testWorkflow.ts
@@ -1,70 +1,48 @@
 import {
-  // WorkflowTrigger,
-  // accessTokenCustomClaims,
-  createKindeAPI,
+  WorkflowTrigger,
+  accessTokenCustomClaims,
+  onUserTokenGeneratedEvent,
+  fetch,
 } from '@kinde/infrastructure';
 
-// export const workflowSettings = {
-//   id: 'addAccessTokenClaim',
-//   trigger: WorkflowTrigger.UserTokenGeneration,
-//   bindings: {
-//     'kinde.accessToken': {},
-//     'kinde.localization': {},
-//     'kinde.fetch': {},
-//     'kinde.env': {},
-//     'kinde.mfa': {},
-//     url: {},
-//   },
-// };
-
 export const workflowSettings = {
-  id: "m2mTokenGeneration",
-  name: "M2M custom claims",
-  failurePolicy: {
-    action: "stop",
-  },
-  trigger: "m2m:token_generation",
+  id: 'addAccessTokenClaim',
+  trigger: WorkflowTrigger.UserTokenGeneration,
   bindings: {
-    "kinde.m2mToken": {}, // required to modify M2M access token
-    "kinde.fetch": {}, // Required for external API calls
-    "kinde.env": {}, // required to access your environment variables
-    url: {}, // required for url params
+    'kinde.accessToken': {},
+    'kinde.localization': {},
+    'kinde.fetch': {},
+    'kinde.env': {},
+    'kinde.mfa': {},
+    url: {},
   },
 };
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default async function TestWorkflow(event:any) {
-  // const accessToken = accessTokenCustomClaims<{
-  //   email: string;
-  //   user_properties: {
-  //     customer_id: {
-  //       v: string;
-  //     };
-  //   };
-  // }>();
+export default async function TestWorkflow(event: onUserTokenGeneratedEvent) {
+  const accessToken = accessTokenCustomClaims<{
+    email: string;
+    user_properties: {
+      customer_id: {
+        v: string;
+      };
+    };
+  }>();
 
-  const kindeAPI = await createKindeAPI(event);
-
-  const { data } = await kindeAPI.get({
-    endpoint: `applications/6c0470e618ab41cfb0cde02661df8734/properties`,
-  });
-  const {appProperties} = data;
-
-  console.log({appProperties})
-
+  console.log({ event });
+  const body = new URLSearchParams();
+  body.append('email', accessToken.email);
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  // const { data }: any = await fetch(
-  //   'https://get-kwh-customer-by-email.netlify.app/get-user',
-  //   {
-  //     method: 'GET',
-  //     body: JSON.stringify({
-  //       email: accessToken.email,
-  //     }),
-  //   }
-  // );
+  const { data }: any = await fetch(
+    'https://get-kwh-customer-by-email.netlify.app/get-user',
+    {
+      method: 'GET',
+      headers:{},
+      body
+    }
+  );
 
-  // console.log('response', data);
+  console.log('response', data);
   // // accessToken.email
 
-  // accessToken.user_properties.customer_id.v = data?.body?.id;
+  accessToken.user_properties.customer_id.v = data?.body?.id;
 }
