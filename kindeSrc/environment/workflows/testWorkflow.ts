9aa6cef9a63d584c7f13d7013bf6e5d8c93b3c42
ankush <ankush@compose.co.in>
2025-04-01 18:17:16 +0530

    feat: add new test workflow for accessToken with customerId claim

---
+++ kindeSrc/environment/workflows/testWorkflow.ts
@@ -0,0 +1,26 @@
+import {
+  WorkflowTrigger,
+  accessTokenCustomClaims,
+} from '@kinde/infrastructure';
+
+export const workflowSettings = {
+  id: 'addAccessTokenClaim',
+  trigger: WorkflowTrigger.UserTokenGeneration,
+  bindings: {
+    'kinde.accessToken': {},
+    'kinde.localization': {},
+    'kinde.fetch': {},
+    'kinde.env': {},
+    'kinde.mfa': {},
+    url: {},
+  },
+};
+
+export default async function TestWorkflow() {
+  const accessToken = accessTokenCustomClaims<{
+    customerId: string;
+  }>();
+
+
+  accessToken.customerId = 'test-customerId'
+}
