55479574574ff125cef3309c12e34ea28a84ae77
ankush <ankush@compose.co.in>
2025-04-04 10:19:08 +0530

    fix: update DefaultPage to use a static logo link and add back navigation with icon

--- kindeSrc/environment/pages/(kinde)/(default)/page.tsx
+++ kindeSrc/environment/pages/(kinde)/(default)/page.tsx
@@ -17,7 +17,31 @@ const DefaultPage: React.FC<KindePageEvent> = ({ context, request }) => {
       <div className='container'>
         <main className='login-form-wrapper'>
           <div className='login-form'>
-            <a href={`${process.env.KINDE_URL}/`}>
+            <a
+              href='https://kwh-kitchenwarehouse.netlify.app/'
+              style={{ display: 'flex', gap: '4px' }}
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='24'
+                height='24'
+                viewBox='0 0 24 24'
+                fill='none'
+                stroke='currentColor'
+                stroke-width='2'
+                stroke-linecap='round'
+                stroke-linejoin='round'
+                className='icon icon-tabler icons-tabler-outline icon-tabler-circle-arrow-left'
+              >
+                <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+                <path d='M12 21a9 9 0 1 0 0 -18a9 9 0 0 0 0 18' />
+                <path d='M8 12l4 4' />
+                <path d='M8 12h8' />
+                <path d='M12 8l-4 4' />
+              </svg>
+              Back to site
+            </a>
+            <a href='https://kwh-kitchenwarehouse.netlify.app/'>
               <div className='logo-wrapper'>
                 <picture>
                   <source
