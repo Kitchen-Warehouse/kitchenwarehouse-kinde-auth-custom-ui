31063581ed443fcb0b5cb86728b37dc3bdbec542
ankush <ankush@compose.co.in>
2025-04-10 21:43:05 +0530

    add: RTE fields on the login and signup forms

--- kindeSrc/environment/pages/(kinde)/(default)/page.tsx
+++ kindeSrc/environment/pages/(kinde)/(default)/page.tsx
@@ -18,8 +18,15 @@ const DefaultPage: React.FC<KindePageEvent> = async ({ context, request }) => {
       method: 'GET',
     }
   );
-  const loginPageImage = res?.data?.results?.[0]?.data?.loginPageImage || null;
+  const {
+    loginPageImage,
+    signInFormTextTop,
+    signupFormTextTop,
+    signInFormTextBottom,
+    signupFormTextBottom,
+  } = res?.data?.results?.[0]?.data || {};
   console.log('res', context, request);
+  const isUserOnLoginOrRegisterPage = request?.route?.flow;
   return (
     <Layout
       context={context}
@@ -28,46 +35,52 @@ const DefaultPage: React.FC<KindePageEvent> = async ({ context, request }) => {
     >
       <div className='container'>
         <main className='login-form-wrapper'>
+          {signInFormTextTop && isUserOnLoginOrRegisterPage === 'login' && (
+            <div
+              className='signInFormTextTopText'
+              dangerouslySetInnerHTML={{ __html: `${signInFormTextTop}` }}
+            />
+          )}
+          {signupFormTextTop && isUserOnLoginOrRegisterPage === 'register' && (
+            <div
+              className='signupFormTextTopText'
+              dangerouslySetInnerHTML={{ __html: `${signupFormTextTop}` }}
+            />
+          )}
           <div className='login-form'>
-            {/* <a
-              href='https://kwh-kitchenwarehouse.netlify.app/'
-              className='back-to-site'
-            >
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                width='24'
-                height='24'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                strokeWidth='2'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                className='icon icon-tabler icons-tabler-outline icon-tabler-circle-arrow-left'
-              >
-                <path stroke='none' d='M0 0h24v24H0z' fill='none' />
-                <path d='M12 21a9 9 0 1 0 0 -18a9 9 0 0 0 0 18' />
-                <path d='M8 12l4 4' />
-                <path d='M8 12h8' />
-                <path d='M12 8l-4 4' />
-              </svg>
-              Back to site
-            </a> */}
-
             {context.widget.content.heading && (
               <h2 className='heading'>{context.widget.content.heading}</h2>
             )}
             <p className='description'>{context.widget.content.description}</p>
             {getKindeWidget()}
           </div>
+          {signInFormTextBottom && isUserOnLoginOrRegisterPage === 'login' && (
+            <div
+              className='signInFormTextBottomText'
+              dangerouslySetInnerHTML={{ __html: `${signInFormTextBottom}` }}
+            />
+          )}
+          {signupFormTextBottom &&
+            isUserOnLoginOrRegisterPage === 'register' && (
+              <div
+                className='signupFormTextBottom'
+                dangerouslySetInnerHTML={{ __html: `${signupFormTextBottom}` }}
+              />
+            )}
         </main>
         {loginPageImage && (
           <div className='side-panel'>
-            <img
-              className='side-panel-image'
-              src={loginPageImage}
-              alt='image'
-            />
+            <picture>
+              <source
+                media='(prefers-color-scheme: dark)'
+                srcSet={loginPageImage}
+              />
+              <img
+                className='side-panel-image'
+                src={loginPageImage}
+                alt='image'
+              />
+            </picture>
           </div>
         )}
       </div>
--- kindeSrc/environment/pages/layout.tsx
+++ kindeSrc/environment/pages/layout.tsx
@@ -25,8 +25,8 @@ export const Layout = ({
   children,
   props,
 }: LayoutProps): React.JSX.Element => {
-  console.log('props', props);
   const { logo, helpText, helpNumber, paymentLogos } = props ?? {};
+
   return (
     <html lang={request.locale.lang}>
       <head>
@@ -39,38 +39,58 @@ export const Layout = ({
         {getKindeRequiredJS()}
         <style nonce={getKindeNonce()}>
           {`
-          .header-container {
-            padding-left: 1.25rem;
-            padding-right: 1.25rem;
-            z-index: 300;
-            padding-top: 1rem;
-            padding-bottom: 0.5rem;
-            margin-left: 0;
-            margin-right: 0;
-            margin-bottom: 1.5rem;
-          }
-
-          .header-content{
-            margin: 0 auto;
-            max-width: 1200px;
-            display: flex;
-            width: 100%;
-            justify-content: space-between;
-            align-items:center;
-            padding: 0.5rem 0;
-            @media screen and (max-width: 1023px) {
-              padding: 0.25rem 0;
+            .header-container {
+              padding-left: 1.25rem;
+              padding-right: 1.25rem;
+              z-index: 300;
+              padding-top: 1rem;
+              padding-bottom: 0.5rem;
+              margin-left: 0;
+              margin-right: 0;
+              margin-bottom: 1.5rem;
             }
-          }
 
-           .container {
+            .header-content{
+              margin: 0 auto;
+              max-width: 1200px;
+              display: flex;
+              width: 100%;
+              justify-content: space-between;
+              align-items:center;
+              padding: 0.5rem 0;
+              @media screen and (max-width: 1023px) {
+                padding: 0.25rem 0;
+              }
+            }
+
+            .secure-login-container{
+              display: flex;
+              align-items: center;
+              gap: 0.5rem;
+            }
+
+            .secure-login {
+              font-family: Inter, sans-serif;
+              color: #666666;
+              font-size: 0.875rem;
+              line-height: 1.25rem;
+              letter-spacing: -0.14px;
+              margin: 0;
+              font-weight: 600;
+            }
+
+            .content-container {
+              display:flex;
+              flex: 1 0 0;
+            }
+
+            .container {
               display: flex;
               width: 100%;
               justify-content: center;
               @media screen and (max-width: 1023px) {
                 flex-direction: column;
                 padding: 20px;
-                height: calc(100vh - 232px);
               }
             }
 
@@ -84,10 +104,17 @@ export const Layout = ({
 
             .login-form-wrapper {
               display: flex;
+              flex-direction: column;
               width: 100%;
-              gap: 120px;
+              gap: 60px;
               justify-content: center;
+              align-items: center;
               flex: 1 0 0;
+            }
+
+            .signInFormTextTopText, .signupFormTextTopText, .signInFormTextBottomText, .signupFormTextBottom {
+                color: #1A1A1A;
+                font-family: Inter, sans-serif;
             }
 
             .login-form {
@@ -95,7 +122,7 @@ export const Layout = ({
               flex-direction: column;
               gap: 24px;
               width: 100%;
-              max-width: 480px;
+              max-width: 578px;
               font-family: Inter, sans-serif;
             }
 
@@ -172,67 +199,130 @@ export const Layout = ({
               }
               .side-panel-image {
                 width: 100%;
-                height: calc(100vh - 240px);
                 object-fit: cover;
               }
             }
           `}
         </style>
       </head>
-      {logo && (
-        <header>
-          <div className='header-container'>
-            <div className='header-content'>
-              <a href='https://kwh-kitchenwarehouse.netlify.app/'>
-                <div className='logo-wrapper'>
-                  <picture>
-                    <source
-                      media='(prefers-color-scheme: dark)'
-                      srcSet={logo}
-                    />
-                    <img
-                      className='logo'
-                      src={logo}
-                      alt={context.widget.content.logoAlt}
-                      width={152}
-                      height={32}
-                    />
-                  </picture>
+      <body style={{ display: 'flex', flexDirection: 'column' }}>
+        {logo && (
+          <header>
+            <div className='header-container'>
+              <div className='header-content'>
+                <a href='https://kwh-kitchenwarehouse.netlify.app/'>
+                  <div className='logo-wrapper'>
+                    <picture>
+                      <source
+                        media='(prefers-color-scheme: dark)'
+                        srcSet={logo}
+                      />
+                      <img
+                        className='logo'
+                        src={logo}
+                        alt={context.widget.content.logoAlt}
+                        width={152}
+                        height={32}
+                      />
+                    </picture>
+                  </div>
+                </a>
+                <div className='secure-login-container'>
+                  <div className='secure-login'>Secure login</div>
+                  <svg
+                    width='24'
+                    height='24'
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    stroke-width='1.5'
+                  >
+                    <path
+                      d='M16.5 10.5V6.75C16.5 4.26472 14.4853 2.25 12 2.25C9.51472 2.25 7.5 4.26472 7.5 6.75V10.5M6.75 21.75H17.25C18.4926 21.75 19.5 20.7426 19.5 19.5V12.75C19.5 11.5074 18.4926 10.5 17.25 10.5H6.75C5.50736 10.5 4.5 11.5074 4.5 12.75V19.5C4.5 20.7426 5.50736 21.75 6.75 21.75Z'
+                      stroke='currentColor'
+                      stroke-width='1.5'
+                      stroke-linecap='round'
+                      stroke-linejoin='round'
+                    ></path>
+                  </svg>
                 </div>
-              </a>
+              </div>
             </div>
-          </div>
-        </header>
-      )}
-      <body>
-        <div data-roast-root='true' data-kinde-root='true'>
+          </header>
+        )}
+        <main
+          data-roast-root='true'
+          data-kinde-root='true'
+          className='content-container'
+        >
           {children}
-        </div>
-      </body>
-      <footer>
-        <div style={{ paddingBottom: '2.5rem' }}>
-          {/* separator */}
-          <div
-            style={{
-              borderTop: '1px solid #e6e6e6',
-              width: '100%',
-              marginTop: '1.5rem',
-            }}
-          />
-          <div
-            style={{ display: 'flex', flexDirection: 'column', gap: '0.75rem' }}
-          >
-            {/* need help */}
+        </main>
+        <footer>
+          <div style={{ paddingBottom: '2.5rem' }}>
+            {/* separator */}
+            <div
+              style={{
+                borderTop: '1px solid #e6e6e6',
+                width: '100%',
+              }}
+            />
             <div
               style={{
                 display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                gap: '0.5rem',
-                paddingTop: '1.5rem',
+                flexDirection: 'column',
+                gap: '0.75rem',
               }}
             >
-              {helpText && (
+              {/* need help */}
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  gap: '0.5rem',
+                  paddingTop: '1.5rem',
+                }}
+              >
+                {helpText && (
+                  <span
+                    style={{
+                      fontFamily: 'Inter, sans-serif',
+                      fontSize: '0.875rem',
+                      lineHeight: '1.25rem',
+                      letterSpacing: '-0.14px',
+                      margin: '0',
+                      color: '#1a1a1a',
+                    }}
+                  >
+                    {helpText}
+                  </span>
+                )}
+                {helpNumber && (
+                  <div>
+                    <a href={`tel:${helpNumber}`} style={{ color: '#1a1a1a' }}>
+                      <span
+                        style={{
+                          fontFamily: 'Inter, sans-serif',
+                          fontSize: '0.875rem',
+                          lineHeight: '1.25rem',
+                          letterSpacing: '-0.14px',
+                          margin: '0',
+                          fontWeight: '600',
+                        }}
+                      >
+                        {helpNumber}
+                      </span>
+                    </a>
+                  </div>
+                )}
+              </div>
+              {/* customer reference */}
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                }}
+              >
                 <span
                   style={{
                     fontFamily: 'Inter, sans-serif',
@@ -243,90 +333,58 @@ export const Layout = ({
                     color: '#1a1a1a',
                   }}
                 >
-                  {helpText}
+                  Your customer reference #:
                 </span>
-              )}
-              {helpNumber && (
-                <div>
-                  <a href={`tel:${helpNumber}`} style={{ color: '#1a1a1a' }}>
-                    <span
-                      style={{
-                        fontFamily: 'Inter, sans-serif',
-                        fontSize: '0.875rem',
-                        lineHeight: '1.25rem',
-                        letterSpacing: '-0.14px',
-                        margin: '0',
-                        fontWeight: '600',
-                      }}
-                    >
-                      {helpNumber}
-                    </span>
-                  </a>
+                <span
+                  style={{
+                    fontSize: '0.875rem',
+                    lineHeight: '1.25rem',
+                    letterSpacing: '-0.14px',
+                    margin: '0',
+                    color: '#1a1a1a',
+                  }}
+                ></span>
+              </div>
+              {/* logos */}
+              <div>
+                <div
+                  style={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    gap: '0.5rem',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                >
+                  {paymentLogos?.map(
+                    (logo: { name: string; image: string }, index: number) => {
+                      return (
+                        logo?.image && (
+                          <div key={index}>
+                            <picture>
+                              <source
+                                media='(prefers-color-scheme: dark)'
+                                srcSet={logo.image}
+                              />
+                              <img
+                                alt={logo.name}
+                                loading='lazy'
+                                width='37'
+                                height='23'
+                                src={logo.image}
+                              />
+                            </picture>
+                          </div>
+                        )
+                      );
+                    }
+                  )}
                 </div>
-              )}
-            </div>
-            {/* customer reference */}
-            <div
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-              }}
-            >
-              <span
-                style={{
-                  fontFamily: 'Inter, sans-serif',
-                  fontSize: '0.875rem',
-                  lineHeight: '1.25rem',
-                  letterSpacing: '-0.14px',
-                  margin: '0',
-                  color: '#1a1a1a',
-                }}
-              >
-                Your customer reference #:
-              </span>
-              <span
-                style={{
-                  fontSize: '0.875rem',
-                  lineHeight: '1.25rem',
-                  letterSpacing: '-0.14px',
-                  margin: '0',
-                  color: '#1a1a1a',
-                }}
-              ></span>
-            </div>
-            {/* logos */}
-            <div>
-              <div
-                style={{
-                  display: 'flex',
-                  flexWrap: 'wrap',
-                  gap: '0.5rem',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}
-              >
-                {paymentLogos?.map(
-                  (logo: { name: string; image: string }, index: number) => {
-                    return (
-                      logo?.image && (
-                        <div key={index}>
-                          <img
-                            alt={logo.name}
-                            loading='lazy'
-                            width='62'
-                            height='40'
-                            src={logo.image}
-                          />
-                        </div>
-                      )
-                    );
-                  }
-                )}
               </div>
             </div>
           </div>
-        </div>
-      </footer>
+        </footer>
+      </body>
     </html>
   );
 };
