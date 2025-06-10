30a621800e8bb861a5a13652deb8b3d7af377847
ankush <ankush@compose.co.in>
2025-06-10 11:10:21 +0530

    fix: refactor M2MToken request to use URLSearchParams for better readability

--- kindeSrc/environment/workflows/testWorkflow.ts
+++ kindeSrc/environment/workflows/testWorkflow.ts
@@ -38,14 +38,25 @@ export default async function TestWorkflow(event: onPostAuthenticationEvent) {
 //       body,
 //     }
 //   );
+
+
+const urlencoded = new URLSearchParams();
+urlencoded.append("grant_type", "client_credentials");
+urlencoded.append("client_id", "6c0470e618ab41cfb0cde02661df8734");
+urlencoded.append("client_secret", "sfM0FxafvSY3WdhttflhOrsva9Q5T0rB2NkxYMWoHXSG03k8tzkW");
+urlencoded.append("audience", "https://kitchenwarehouse-staging.au.kinde.com/api");
+const requestOptions = {
+  method: "POST" as "POST" | "GET" | "PUT" | "DELETE" | "PATCH",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+  },
+  body: urlencoded,
+  redirect: "follow"
+};
 console.log({isNewKindeUser})
     const M2MToken = await fetch(
       'https://auth-staging.kitchenwarehouse.com.au/oauth2/token',
-      {
-        method: 'POST',
-               responseFormat: 'text',
-        headers: {},
-      }
+      requestOptions
     );
 console.log('M2MToken', M2MToken.data);
 //   console.log('response', response?.data);
