a1a2cb5331b12b6f48075fd03be197544111e2c2
ankush <ankush@compose.co.in>
2025-08-14 13:07:23 +0530

    fix: update Content-Security-Policy for enhanced security and resource loading

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: "default-src * 'unsafe-inline' 'unsafe-eval';",
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' kitchenwarehouse.com.au *.kitchenwarehouse.com.au https://js.stripe.com https://auth-staging.kitchenwarehouse.com.au https://cdn.jsdelivr.net https://cdnjs.cloudflare.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https: res.cloudinary.com; font-src 'self' data: https:; connect-src 'self' kitchenwarehouse.com.au *.kitchenwarehouse.com.au;",
           },
         ],
       },
