50a1ba76c9f83074b4c7d722c50837add4de5731
ankush <ankush@compose.co.in>
2025-09-08 11:40:50 +0530

    refactor: comment out unused imports and functions in TestWorkflow and TestWorkflow1

--- kindeSrc/environment/workflows/testWorkflow.ts
+++ kindeSrc/environment/workflows/testWorkflow.ts
@@ -1,10 +1,10 @@
-import {
-  WorkflowTrigger,
-  accessTokenCustomClaims,
-  fetch,
-  onUserTokenGeneratedEvent,
-  // onPostAuthenticationEvent,
-} from '@kinde/infrastructure';
+// import {
+//   WorkflowTrigger,
+//   accessTokenCustomClaims,
+//   fetch,
+//   onUserTokenGeneratedEvent,
+//   // onPostAuthenticationEvent,
+// } from '@kinde/infrastructure';
 
 // export const workflowSettings = {
 //   id: 'postAuthentication',
@@ -44,56 +44,56 @@ import {
 //   // Need email ID (can be in event)
 // }
 
-export const workflowSettings = {
-  id: 'userTokenGeneration',
-  trigger: WorkflowTrigger.UserTokenGeneration,
-  bindings: {
-    'kinde.accessToken': {},
-    'kinde.localization': {},
-    'kinde.fetch': {},
-    'kinde.env': {},
-    'kinde.mfa': {},
-    url: {},
-  },
-};
+// export const workflowSettings = {
+//   id: 'userTokenGeneration',
+//   trigger: WorkflowTrigger.UserTokenGeneration,
+//   bindings: {
+//     'kinde.accessToken': {},
+//     'kinde.localization': {},
+//     'kinde.fetch': {},
+//     'kinde.env': {},
+//     'kinde.mfa': {},
+//     url: {},
+//   },
+// };
 
-export default async function TestWorkflow(event: onUserTokenGeneratedEvent) {
-  const accessToken = accessTokenCustomClaims<{
-      customer_id?: string;
+// export default async function TestWorkflow(event: onUserTokenGeneratedEvent) {
+//   const accessToken = accessTokenCustomClaims<{
+//       customer_id?: string;
 
-    user_properties?: {
-      customer_id?: string;
-    }
-  }>();
-  const userId = event.context.user.id;
-  console.log({ userId });
+//     user_properties?: {
+//       customer_id?: string;
+//     }
+//   }>();
+//   const userId = event.context.user.id;
+//   console.log({ userId });
   
-  const customerData = await getCustomerByKindeId(userId);
-  console.log({ customerData, customerId: customerData?.data?.customer?.id, accessToken: accessToken?.user_properties?.customer_id });
-  if(customerData?.data?.customer?.id){
-    accessToken.customer_id = customerData.data.customer.id;
-    console.log('Populated the customer id');
-  } else {
-    console.log('Customer ID not found');
-  }
-}
+//   const customerData = await getCustomerByKindeId(userId);
+//   console.log({ customerData, customerId: customerData?.data?.customer?.id, accessToken: accessToken?.user_properties?.customer_id });
+//   if(customerData?.data?.customer?.id){
+//     accessToken.customer_id = customerData.data.customer.id;
+//     console.log('Populated the customer id');
+//   } else {
+//     console.log('Customer ID not found');
+//   }
+// }
 
-async function getCustomerByKindeId(kindeCustomerId: string) {
-  try {
-    const response = await fetch(
-      `https://kwh-kitchenwarehouse.frontastic.rocks/frontastic/action/account/getcustomerbykindeid?kindeCustomerId=${kindeCustomerId}`,
-      {
-        method: 'POST',
-        headers: {
-          'Commercetools-Frontend-Extension-Version': 'devnarendra',
-          Accept: 'application/json',
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-      }
-    );
-    return response;
-  } catch (error) {
-    console.error('Error fetching customer by Kinde ID:', error);
-    throw error;
-  }
-}
+// async function getCustomerByKindeId(kindeCustomerId: string) {
+//   try {
+//     const response = await fetch(
+//       `https://kwh-kitchenwarehouse.frontastic.rocks/frontastic/action/account/getcustomerbykindeid?kindeCustomerId=${kindeCustomerId}`,
+//       {
+//         method: 'POST',
+//         headers: {
+//           'Commercetools-Frontend-Extension-Version': 'devnarendra',
+//           Accept: 'application/json',
+//           'Content-Type': 'application/x-www-form-urlencoded',
+//         },
+//       }
+//     );
+//     return response;
+//   } catch (error) {
+//     console.error('Error fetching customer by Kinde ID:', error);
+//     throw error;
+//   }
+// }
